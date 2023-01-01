e661a44ddb98b3c9e687467995b9b36ac738d644
Allan Chain <36528777+AllanChain@users.noreply.github.com>
2023-01-01 20:49:42 +0800

    fix: modal scroll bar
    Previously, both the outsider and insider can scroll

--- src/App.vue
+++ src/App.vue
@@ -7,47 +7,52 @@ import BlockedItem from './components/BlockedItem.vue'
 <template>
   <div
     v-if="showSettings"
-    un-position="fixed top-1/2 left-1/2"
-    un-transform="translate-y--1/2 translate-x--1/2"
-    un-w-150 un-h-60 un-z-200
-    un-max-w="11/12"
-    un-border="rounded-lg t-10 orange-400"
-    un-overflow="y-auto"
-    un-bg="gray-100/90"
-    un-text="xs"
+    un-position="fixed top-0 left-0"
+    un-z-200 un-w-full un-h-full
+    un-bg="black/30"
   >
     <div
-      un-position="absolute top-2 right-2"
-      @click="showSettings = false"
+      un-position="relative top-1/2 left-1/2"
+      un-transform="translate-y--1/2 translate-x--1/2"
+      un-w-150 un-z-200
+      un-max-w="11/12"
+      un-border="rounded-lg t-10 orange-400"
+      un-bg="gray-100/90"
+      un-text="xs"
     >
-      <div class="i-carbon-close text-lg" />
-    </div>
-    <div un-m="x-4" un-overflow="y-auto">
-      <div un-text="sm" un-font="bold" un-m="y-2">
-        已屏蔽的用户
+      <div
+        un-position="absolute top-2 right-2"
+        @click="showSettings = false"
+      >
+        <div class="i-carbon-close text-lg" />
       </div>
-      <div un-flex un-flex-wrap>
-        <BlockedItem
-          v-for="blockedUser in blockedUsers"
-          :key="blockedUser"
-          :name="blockedUser"
-          @click="unblockUser(blockedUser)"
-        />
+      <div un-p="x-4" un-max-h="60" un-overflow="y-auto">
+        <div un-text="sm" un-font="bold" un-m="y-2">
+          已屏蔽的用户
+        </div>
+        <div un-flex un-flex-wrap>
+          <BlockedItem
+            v-for="blockedUser in blockedUsers"
+            :key="blockedUser"
+            :name="blockedUser"
+            @click="unblockUser(blockedUser)"
+          />
+        </div>
+        <div un-text="sm" un-font="bold" un-m="y-2">
+          已屏蔽的版面
+        </div>
+        <div un-flex un-flex-wrap>
+          <BlockedItem
+            v-for="blockedBoard in blockedBoards"
+            :key="blockedBoard"
+            :name="blockedBoard"
+            @click="unblockBoard(blockedBoard)"
+          />
+        </div>
+        <p un-text="xs gray-600">
+          取消屏蔽后需要刷新页面才能生效
+        </p>
       </div>
-      <div un-text="sm" un-font="bold" un-m="y-2">
-        已屏蔽的版面
-      </div>
-      <div un-flex un-flex-wrap>
-        <BlockedItem
-          v-for="blockedBoard in blockedBoards"
-          :key="blockedBoard"
-          :name="blockedBoard"
-          @click="unblockBoard(blockedBoard)"
-        />
-      </div>
-      <p un-text="xs gray-600">
-        取消屏蔽后需要刷新页面才能生效
-      </p>
     </div>
   </div>
 </template>
